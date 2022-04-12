bc883dd13cda79380d5f34ce7cc888671a7270ae
Pritam-debnath <pritamd@geekyants.com>
2022-04-12 17:17:41 +0530

    map is rendered

--- lists/lib/main.dart
+++ lists/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './customListTile.dart';
+
 void main() {
   runApp(ListsRender());
 }
@@ -10,12 +12,63 @@ class ListsRender extends StatefulWidget {
 }
 
 class _ListRenderState extends State<ListsRender> {
+  var lists = [
+    {
+      'name': 'Akira',
+      'description':
+          'Japan, 1988. An explosion caused by a young boy with psychic powers....'
+    },
+    {
+      'name': 'Perfect Blue',
+      'description': 'J-pop idol group CHAM! has spent....'
+    },
+    {
+      'name': 'Koe no Katachi',
+      'description':
+          'As a wild youth, elementary school student Shouya Ishida...'
+    },
+    {
+      'name': 'Violet Evergarden',
+      'description':
+          'As a wild youth, elementary school student Shouya Ishida...'
+    },
+    {
+      'name': 'Kimi no nawa',
+      'description':
+          'As a wild youth, elementary school student Shouya Ishida...'
+    },
+  ];
+  // var lists = [
+  //   'Akira',
+  //   'Perfect Blue',
+  // ];
+
+  var index = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Lists'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.separated(
+                separatorBuilder: (BuildContext context, int index) => Divider(
+                  color: Colors.black,
+                  endIndent: 10,
+                  indent: 10,
+                ),
+                itemCount: lists.length,
+                itemBuilder: (context, index) {
+                  final name = lists[index]['name'];
+                  final desc = lists[index]['description'];
+                  return CustomListTile(name as String, desc as String);
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
