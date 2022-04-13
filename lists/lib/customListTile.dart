6e46588e6243bb8382334db6319970c4907de60d
Pritam-debnath <pritamd@geekyants.com>
2022-04-13 07:31:58 +0530

    formatted and styled the texts, description and images

--- lists/lib/customListTile.dart
+++ lists/lib/customListTile.dart
@@ -1,32 +1,50 @@
 import 'package:flutter/material.dart';
 
 class CustomListTile extends StatelessWidget {
-  final String name, desc, img;
-  CustomListTile(this.name, this.desc, this.img);
+  final String name, desc, img, year;
+  CustomListTile(this.name, this.desc, this.img, this.year);
   @override
   Widget build(BuildContext context) {
     print('img ${img}');
     return Center(
       child: Row(
         children: [
-          Image(
-            image: AssetImage('images/' + img),
+          Container(
+            child: CircleAvatar(
+              backgroundImage: AssetImage('images/' + img),
+              radius: 100,
+            ),
             height: 100,
             width: 100,
           ),
-          Column(
-            children: [
-              Text(
-                name,
-                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-              Text(
-                desc,
-                style: TextStyle(fontSize: 14),
-                textAlign: TextAlign.center,
-              ),
-            ],
+          SizedBox(
+            height: 210,
+            width: 250,
+            child: Card(
+                child: Column(
+              children: [
+                ListTile(
+                  title: Text(
+                    name,
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.left,
+                  ),
+                  subtitle: Text(
+                    'Year: ' + year,
+                    // style: TextStyle(fontSize: 14),
+                    // textAlign: TextAlign.left,
+                  ),
+                ),
+                const Divider(),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    desc,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            )),
           ),
         ],
       ),
--- lists/lib/main.dart
+++ lists/lib/main.dart
@@ -15,28 +15,36 @@ class _ListRenderState extends State<ListsRender> {
   var lists = [
     {
       'name': 'Akira',
-      'description': 'Japan, 1988. An explosion caused ....',
+      'description':
+          'Japan, 1988. An explosion caused by a young boy with psychic powers tears through the city of Tokyo and ignites the fuse that leads to World War III.',
       'image': 'akira.jpg',
+      'year': '1988'
     },
     {
       'name': 'Perfect Blue',
-      'description': 'J-pop idol group CHAM! has spent....',
-      'image': 'kiminonawa.jpg',
+      'description':
+          'J-pop idol group CHAM! has spent the last two years entertaining its fans. Sadly, all good things must come to an end, and CHAM! must see one of its members, Mima Kirigoe, leave the group to pursue her acting career. While Mima\'s choice is met with a mixed response, she hopes her fans will continue to support her.',
+      'image': 'perfectblue.jpg',
+      'year': '1998'
     },
     {
       'name': 'Koe no Katachi',
       'description': 'As a wild youth, elementary...',
       'image': 'koenokatachi.jpg',
+      'year': '2018'
     },
     {
       'name': 'Violet Evergarden',
       'description': 'As a wild youth, elementary...',
-      'image': 'perfectblue.jpg',
+      'image': 'violet.jpg',
+      'year': '2021'
     },
     {
       'name': 'Kimi no nawa',
-      'description': 'As a wild youth, elementary...',
-      'image': 'violet.jpg',
+      'description':
+          'Mitsuha Miyamizu, a high school girl, yearns to live the life of a boy in the bustling city of Tokyo—a dream that stands in stark contrast to her present life in the countryside. Meanwhile in the city, Taki Tachibana lives a busy life as a high school student while juggling his part-time job and hopes for a future in architecture.',
+      'image': 'kiminonawa.jpg',
+      'year': '2016'
     },
   ];
   // var lists = [
@@ -66,8 +74,9 @@ class _ListRenderState extends State<ListsRender> {
                   final name = lists[index]['name'];
                   final desc = lists[index]['description'];
                   final img = lists[index]['image'];
-                  return CustomListTile(
-                      name as String, desc as String, img as String);
+                  final year = lists[index]['year'];
+                  return CustomListTile(name as String, desc as String,
+                      img as String, year as String);
                 },
               ),
             ),
