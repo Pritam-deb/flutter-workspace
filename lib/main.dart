60740ce04361074f592835641f12a6863e887df7
Pritam-debnath <pritamd@geekyants.com>
2022-04-13 17:52:02 +0530

    completed and understood the codelab

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,88 @@ class _mainLayoutState extends State<mainLayout> {
       appBar: AppBar(
         title: Text('Layouts Play'),
       ),
+      body: Card(
+        child: Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.account_circle,
+                    size: 60,
+                  ),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "Flutter McFlutter",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      Text(
+                        "Experienced App Developer",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20, bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    Text(
+                      "123 Main Street",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      "(415) 555-0198",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: const [
+                  Icon(
+                    Icons.accessibility,
+                    size: 30,
+                  ),
+                  Icon(
+                    Icons.timer,
+                    size: 30,
+                  ),
+                  Icon(
+                    Icons.phone_android,
+                    size: 30,
+                  ),
+                  Icon(
+                    Icons.phone_iphone,
+                    size: 30,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     ));
   }
 }
