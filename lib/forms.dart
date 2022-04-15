2df92cec7119269abc11a63c82721e84d6d18bed
Pritam-debnath <pritamd@geekyants.com>
2022-04-15 17:16:15 +0530

    added new fields and icons and labels

--- lib/forms.dart
+++ lib/forms.dart
@@ -17,20 +17,75 @@ class _FormsState extends State<Forms> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          TextFormField(
-            decoration: InputDecoration(hintText: 'email dhalo!'),
-            validator: (String? value) {
-              if (value == null || value.isEmpty) {
-                return 'kuch bhi dhalo';
-              }
-              return null;
-            },
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'First Name',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.person)),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Enter your first name';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'Last name',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.person)),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Enter your last name';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email)),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Enter your email';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'Create password',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.key)),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Create a password';
+                }
+                return null;
+              },
+            ),
           ),
           Padding(
             padding: EdgeInsets.symmetric(vertical: 26.0),
             child: ElevatedButton(
                 onPressed: () {
-                  if (_formKey.currentState!.validate()) {}
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('data processing')),
+                    );
+                  }
                 },
                 child: Text('Submit email')),
           )
