fdb1b657e12c39e9e2e4b1778aa7f9c9d35e9cbf
Pritam-debnath <pritamd@geekyants.com>
2022-04-20 16:24:00 +0530

    bug in ListView.builer()

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,50 +9,49 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 335,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tr) {
-            return Card(
-                color: Color.fromARGB(255, 238, 215, 255),
-                child: Row(
-                  children: [
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Color.fromARGB(255, 252, 116, 116),
-                            width: 2),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '₹' + tr.amount.toString(),
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Color.fromARGB(255, 252, 116, 116)),
-                      ),
+      height: 230,
+      //try using with LisView children and then with ListView.builder for optimizations
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+              color: Color.fromARGB(255, 238, 215, 255),
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Color.fromARGB(255, 252, 116, 116), width: 2),
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(tr.title,
-                            style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w500,
-                                color: Color.fromARGB(255, 252, 116, 116))),
-                        Text(DateFormat('dd-MMM-yyyy').format(tr.date),
-                            style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w400,
-                                color: Colors.grey))
-                      ],
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '₹' + transactions[index].amount.toString(),
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Color.fromARGB(255, 252, 116, 116)),
                     ),
-                  ],
-                ));
-          }).toList(),
-        ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(transactions[index].title,
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                              color: Color.fromARGB(255, 252, 116, 116))),
+                      Text(
+                          DateFormat('dd-MMM-yyyy')
+                              .format(transactions[index].date),
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.grey))
+                    ],
+                  ),
+                ],
+              ));
+        },
       ),
     );
   }
