04cbfee7425ea935140a1055937a474b85deab3e
Pritam-debnath <pritamd@geekyants.com>
2022-04-28 15:16:36 +0530

    done with fetch API

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -43,19 +43,45 @@ class _HomePageState extends State<HomePage> {
       body: ListView.builder(
         itemBuilder: (context, index) {
           return Container(
-            child: Center(
-              child: Container(
-                height: 200,
-                width: 300,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.amberAccent),
-                child: Center(
-                  child: listResponse == null || supportResponse == null
-                      ? Text("loading the data...")
-                      : Text(
-                          listResponse![index]['first_name'].toString(),
-                        ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: Container(
+                  height: 200,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.amberAccent),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(6),
+                        child: Image.network(listResponse![index]['avatar']),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            child:
+                                listResponse == null || supportResponse == null
+                                    ? Text("loading the data...")
+                                    : Text(
+                                        listResponse![index]['first_name']
+                                                .toString() +
+                                            ' ' +
+                                            listResponse![index]['last_name']
+                                                .toString(),
+                                      ),
+                          ),
+                          Text(
+                            listResponse![index]['email'].toString(),
+                            style: TextStyle(fontSize: 10, color: Colors.grey),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
