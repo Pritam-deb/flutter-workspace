638fd191d08c5edc145cefc765e574358722242c
Pritam-debnath <pritamd@geekyants.com>
2022-05-02 20:01:36 +0530

    completed the app

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -1,2 +1,7 @@
 String apiKey = "c4e81b90210b437a937eadbf55f50740";
 String url = "newsapi.org";
+String title = "MyNEWS";
+String heading = "Top Headlines";
+String location = "LOCATION";
+String hint = "Search for news,topics...";
+String search = "Search";
